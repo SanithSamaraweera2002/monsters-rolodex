c43dabec1418e0fd656b99b1ff5c72ad32a92e3c
White Tangent <63760473+SanithSamaraweera2002@users.noreply.github.com>
2023-02-14 07:30:31 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      monsters: [
+        {
+          name: "Dracula",
+          id: 'ac1'
+        },
+        {
+          name: "Frankestien",
+          id: 'ac2'
+        },
+        {
+          name: "Zombie",
+          id: 'ac3'
+        }
+      ]
+    }
+    
+  }
+
+  render(){
+      return (
+        <div className="App">
+          {
+            this.state.monsters.map(monsters => 
+            <h1 key={monsters.id}>{monsters.name }</h1>)
+          }
+        </div>
+      )
+    }
+  }
+
+
+export default App;
