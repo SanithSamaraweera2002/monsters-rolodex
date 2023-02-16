8f0c8e6fc9101d9e87f8145d19e0c95a4ecfe0a6
White Tangent <63760473+SanithSamaraweera2002@users.noreply.github.com>
2023-02-16 06:31:08 +0530

    Changes DOne

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends Component{
       )
     return (
       <div className="App">
-      <h1>SANITH</h1>
+      <h1>Monsters Rolodex</h1>
       <SearchBox
         placeholder='Search Monster'
         handleChange={e=> 
